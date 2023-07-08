ce9276273b419a59b3c1899bc3d66bb43d315bef
ambrocio-ing <0332142014@unjfsc.edu.pe>
2023-07-08 18:08:15 -0500

    renombrando archivos de configuarcion

--- customer_service/src/main/resources/data.sql
+++ customer_service/src/main/resources/data.sql
@@ -1,3 +1,4 @@
+/*
 INSERT INTO regions (id, name) VALUES (1, 'Sudamérica');
 INSERT INTO regions (id, name) VALUES (2, 'Centroamérica');
 INSERT INTO regions (id, name) VALUES (3, 'Norteamérica');
@@ -8,3 +9,4 @@ INSERT INTO regions (id, name) VALUES (7, 'Oceanía');
 INSERT INTO regions (id, name) VALUES (8, 'Antártida');
 
 INSERT INTO customers (id,number_id,first_name,last_name , email, photo_url, region_id, state) VALUES(1,'32404580', 'Andrés', 'Guzmán', 'profesor@bolsadeideas.com','',1,'CREATED');
+*/
